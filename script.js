73026ae668fc135799090b725bebba9126be7719
Michael McNeil <rmichael.mcneil@gmail.com>
2023-06-02 08:01:54 -0500

    add popup

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Create the Leaflet map
 var map = L.map('map').setView([30.3782, -89.9340], 10);
-// Add the aerial imagery basemap
-var aerialImagery = L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri'
+// Add the custom WMS basemap layer
+var wms2023 = L.tileLayer.wms('https://imagery.geoportalmaps.com/lizardtech/iserv/ows', {
+    layers: 'StTammany2023'
+    , format: 'image/jpeg'
+    , transparent: true
+    , tileSize: 1024
 }).addTo(map);
 // Add the sales parcels layer with customized marker style
 var salesParcels = L.geoJSON(null, {
@@ -18,13 +21,15 @@ var salesParcels = L.geoJSON(null, {
     }
     , onEachFeature: function (feature, layer) {
         // Customize the popup content here
-        layer.bindPopup("Sale Price: $" + feature.properties.SalePrice);
+        layer.bindPopup("<div class='custom-popup'>" + "<span class='popup-label'>Assessment No. " + feature.properties['REID'] + "</span><br><span class='popup-sub-label'>Property Class:</span> " + feature.properties['Property C'] + "<br><span class='popup-sub-label'>Sale Price:</span> $" + feature.properties['Sale Price'] + "<br><span class='popup-sub-label'>Sale Date:</span> " + feature.properties['Sale Date'] + "<br><span class='popup-sub-label'>Document No.:</span> " + feature.properties['Ownership'] + "<br><span class='popup-sub-label'>Sale Type:</span> " + feature.properties['Sale Type'] + "<br><span class='popup-sub-label'>Disqualification Reason:</span> " + feature.properties['Sale Disqu'] + "</div>");
     }
 }).addTo(map);
 // Load the sales parcels GeoJSON data
-fetch("data/2022-sales-parcels-wgs84.geojson").then(response => response.json()).then(data => {
+fetch("data/2022-sales-parcels.geojson").then(response => response.json()).then(data => {
     // Print the GeoJSON data to the console
     console.log(data);
     // Add the GeoJSON data to the sales parcels layer
     salesParcels.addData(data);
+    // Fit the map to the bounds of the sales parcels layer
+    map.fitBounds(salesParcels.getBounds());
 });
